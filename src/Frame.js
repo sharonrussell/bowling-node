563f4a1404b1ff13980fad66b8322986a6a97a86
sharonrussell <sharon.russell@comparethemarket.com>
2016-04-08 19:31:44 +0100

    Code to make the frame tries going down pass

--- src/Frame.js
+++ src/Frame.js
@@ -1,13 +1,20 @@
 function Frame() {
 	this._pins = 10;
+	this._tries = 2;
 }
 
 Frame.prototype.knockPins = function(pins) {
 	this._pins -= pins;
+
+	this._tries--;
 }
 
 Frame.prototype.pins = function() {
 	return this._pins;
+}
+
+Frame.prototype.tries = function() {
+	return this._tries;
 }
 
 module.exports = Frame;
