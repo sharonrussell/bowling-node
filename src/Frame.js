bbec0ddd7f1b8ae0295cd793def1cd08bd60e767
sharonrussell <sharon.russell@comparethemarket.com>
2016-04-08 19:38:50 +0100

    Add code to make test pass if a frame is a spare

--- src/Frame.js
+++ src/Frame.js
@@ -1,16 +1,25 @@
 function Frame() {
 	this._pins = 10;
 	this._tries = 2;
+	this._isSpare = false;
 }
 
 Frame.prototype.knockPins = function(pins) {
 	this._pins -= pins;
+
+	if(this._pins === 0 && this._tries === 1){
+		this._isSpare = true;
+	}
 
 	this._tries--;
 }
 
 Frame.prototype.pins = function() {
 	return this._pins;
+}
+
+Frame.prototype.isSpare = function() {
+	return this._isSpare;
 }
 
 Frame.prototype.tries = function() {
